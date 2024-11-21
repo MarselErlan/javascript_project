e6fd845090cf655428fc9da6e95c92f35d42f58f
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2024-11-21 15:39:58 -0600

    add new function scroll down and up smooth

--- app.js
+++ app.js
@@ -232,31 +232,29 @@
 // }) // callback function will execited
 
 
-// const userList = document.querySelectorAll(".name-list li");
-const userList = document.querySelector(".name-list");
-const listInput = document.querySelector(".list-input");
-const addListBtn = document.querySelector(".addListBtn");
+// // const userList = document.querySelectorAll(".name-list li");
+// const userList = document.querySelector(".name-list");
+// const listInput = document.querySelector(".list-input");
+// const addListBtn = document.querySelector(".addListBtn");
 
-// for(user of userList){
-//     user.addEventListener('click', function(){
-//         // console.log(this); 
-//         this.style.color = 'red';
-//     });
-// }
-
-
-// console.log(listInput.value);
+// // for(user of userList){
+// //     user.addEventListener('click', function(){
+// //         // console.log(this); 
+// //         this.style.color = 'red';
+// //     });
+// // }
 
 
-addListBtn.addEventListener('click', function(){
-// Create an li out of thin air
-const newLi = document.createElement('LI');
-const liContent = document.createTextNode(listInput.value);
-// Add the input value inside that new li
-newLi.appendChild(liContent);
-// Attaching the li to the user list
-userList.appendChild(newLi);
+// // console.log(listInput.value);
 
+// addListBtn.addEventListener('click', function(){
+// // Create an li out of thin air
+// const newLi = document.createElement('LI');
+// const liContent = document.createTextNode(listInput.value);
+// // Add the input value inside that new li
+// newLi.appendChild(liContent);
+// // Attaching the li to the user list
+// userList.appendChild(newLi);
 
-});
+// });
 
